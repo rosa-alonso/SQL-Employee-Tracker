14fe7fb4ecb377c17bc5592f16f0b6ba83e05b1c
Rosa Alonso <alonso_2018@yahoo.com>
2023-05-10 10:03:43 -0500

    added everything from my db file, query, schema, and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (id, name)
+VALUES  (1, "Sales"),
+        (2, "Engineering"),
+        (3, "Finance"),
+        (4, "Legal");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES  (1, "sales lead", 100000.00, 1),
+        (2, "salesperson", 80000.00, 1),
+        (3, "lead engineer", 150000.00, 2),
+        (4, "software engineer", 120000.00, 2),
+        (5, "account manager", 160000.00, 3),
+        (6, "accountant", 125000.00, 3),
+        (7, "legal team lead", 250000.00, 4),
+        (8, "lawyer", 190000.00, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (1, 'John', 'Doe', 1, 1),
+        (2, 'Jack', 'Daniels', ),
+        (3, 'Jane', 'Doe', ),
+        (4, 'Lane', 'Hicks', ),
+        (5, 'Allyson', 'Smith', ),
+        (6, 'May', 'Ronaldo', ),
+        (7, 'Daisy', 'Dukes', ),
+        (8, 'Fred', 'White', ),
+        (9, 'Wayne', 'Johnson', ),
+        (10, 'Darla', 'Williamas', ),
+        (11, 'Tom', 'Jean', ),
+        (12, 'Ben', 'Ten', ),
+        (13, 'Justin', 'Case', ),
+        (14, 'Harry', 'Smith', );
+        
+
+
