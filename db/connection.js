6f17286a07c6c166f7ca7ccadd9af68ed1a0ec77
Rosa Alonso <alonso_2018@yahoo.com>
2023-05-28 19:16:41 -0500

    worked on adding functionality to server.js file

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,17 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+// const inquirer = require("inquirer");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "employees",
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "employeetracker",
+//   password: "",
+// });
 
-connection.connect(function (err) {
-  if (err) throw err;
-});
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("connected as id" + connection.threadId + "\n");
+//   addEmployee();
+// });
 
-module.exports = connection;
+// module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
+const { default: inquirer } = require("inquirer");
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -13,6 +15,58 @@ connection.connect(function (err) {
   console.log("connected as id" + connection.threadId + "\n");
   addEmployee();
 });
+
+const questions = [
+  {
+    type: "list",
+    name: "choice",
+    message: "What would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+    ],
+  },
+];
+function viewDepartments() {
+  queryFunction.findAllDepartments();
+}
+
+function viewAllRoles() {
+  queryFunction.findAllRoles();
+}
+
+function viewAllEmployees() {
+  queryFunction.FindAllEmployees();
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "departmentName",
+        message: "What is the new department name?",
+        type: "input",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "INSERT INTO department SET ?",
+        [
+          {
+            first_name: answers.departmentName,
+          },
+        ],
+        function (err, res) {
+          console.log("department inserted");
+        }
+      );
+    });
+}
 
 function addEmployee() {
   inquirer
@@ -45,3 +99,37 @@ function addEmployee() {
       );
     });
 }
+
+// function updateEmployee() {
+//   queryFunction.FindAllEmployees()
+
+//   inquirer
+//     .prompt([
+//       {
+//         name: "employeeFirstName",
+//         message: "What is the new employees first name?",
+//         type: "input",
+//       },
+//       {
+//         name: "employeeLastName",
+//         message: "What is the new employees last name?",
+//         type: "input",
+//       },
+//     ])
+//     .then((answers) => {
+//       connection.query(
+//         "INSERT INTO role SET ?",
+//         [
+//           {
+//             first_name: answers.employeeFirstName,
+//           },
+//           {
+//             last_name: answers.employeeLastName,
+//           },
+//         ],
+//         function (err, res) {
+//           console.log("employee role updated");
+//         }
+//       );
+//     });
+// }
